a8718b8f29317d6a14f13c15167958ac0a023022
masakasakasama <64453679+masakasakasama@users.noreply.github.com>
2026-06-07 09:08:23 +0900

    Add browser visibility activity

---
+++ app/src/main/java/com/tatsuya/websearch/BrowserVisibilityActivity.kt
@@ -0,0 +1,11 @@
+package com.tatsuya.websearch
+
+import android.app.Activity
+import android.os.Bundle
+
+class BrowserVisibilityActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
